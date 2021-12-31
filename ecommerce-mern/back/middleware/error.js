84d84fc5183ef190e34b853a566f86df3acf1f20
Diego Serafim <diegoserafim1@gmail.com>
2021-12-31 19:11:33 -0300

    tratando erro de id não encontrado

--- ecommerce-mern/back/middleware/error.js
+++ ecommerce-mern/back/middleware/error.js
@@ -1,11 +1,17 @@
-const ErrorHandler = require("../utils/errorhander");
+const ErrorHander = require("../utils/errorhander");
 
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Erro interno do servidor";
+
+    // Erro de id do mongodb errado
+    if (err.name === "CastError") {
+        const message = `Recurso não encontrado. Inválido: ${err.path}`;
+        err = new ErrorHander(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
-        // error: err,
         message: err.message,
     });
 };

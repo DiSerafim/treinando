618de5da54429f5dfdf16834b41baa3699cb23e0
Diego Serafim <diegoserafim1@gmail.com>
2022-02-23 22:11:57 -0300

    created filter price

--- ecommerce-mern/front/src/component/Product/Products.js
+++ ecommerce-mern/front/src/component/Product/Products.js
@@ -4,22 +4,39 @@ import { clearErros, getProduct } from "../../actions/productAction";
 import Loader from "../layout/Loader/Loader";
 import ProductCard from "../Home/ProductCard";
 import Pagination from "react-js-pagination";
+import Slider from "@material-ui/core/Slider";
+import Typography from "@material-ui/core/Typography";
 import "./Products.css";
 
 const Products = ({ match }) => {
     const dispatch = useDispatch();
     const [currentPage, setCurrentPage] = useState(1);
-    const { products, loading, error, productsCount, resultPerPage } = useSelector(
-        (state) => state.products
-    );
+    const [price, setPrice] = useState([0, 25000]);
+
+    const {
+        products,
+        loading,
+        error,
+        productsCount,
+        resultPerPage,
+        filteredProductsCount,
+    } = useSelector((state) => state.products);
+
     const keyword = match.params.keyword;
+
     const setCurrentPageNo = (e) => {
-        setCurrentPage(e)
-    }
+        setCurrentPage(e);
+    };
+
+    const priceHandler = (event, newPrice) => {
+        setPrice(newPrice);
+    };
 
     useEffect(() => {
-        dispatch(getProduct(keyword, currentPage));
-    }, [dispatch, keyword, currentPage]);
+        dispatch(getProduct(keyword, currentPage, price));
+    }, [dispatch, keyword, currentPage, price]);
+
+    let count = filteredProductsCount;
 
     return (
         <Fragment>
@@ -35,8 +52,22 @@ const Products = ({ match }) => {
                             ))
                         }
                     </div>
+                    
+                    {/* filtragem */}
+                    <div className="filterBox">
+                        <Typography>Preços</Typography>
+                        <Slider
+                            value={price}
+                            onChange={priceHandler}
+                            valueLabelDisplay="auto"
+                            aria-labelledby="range-slider"
+                            min={0}
+                            max={25000}
+                        />
+                    </div>
 
-                    {resultPerPage < productsCount && (
+                    {/* paginação */}
+                    {resultPerPage < count && (
                         <div className="paginationBox">
                             <Pagination
                                 activePage={currentPage}
@@ -55,7 +86,7 @@ const Products = ({ match }) => {
                         </div>
                     )}
                 </Fragment>
-            )};
+            )}
         </Fragment>
     );
 };

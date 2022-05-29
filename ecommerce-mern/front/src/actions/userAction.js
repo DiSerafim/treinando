cc751c8dff464e8f4fcf1515a3b8cfa7014eafee
Diego Serafim <diegoserafim1@gmail.com>
2022-05-28 22:23:42 -0300

    created dashboard logout user

--- ecommerce-mern/front/src/actions/userAction.js
+++ ecommerce-mern/front/src/actions/userAction.js
@@ -2,14 +2,19 @@ import {
     LOGIN_SUCCESS,
     LOGIN_REQUEST,
     LOGIN_FAIL,
-    CLEAR_ERRORS,
     REGISTER_USER_REQUEST,
     REGISTER_USER_SUCCESS,
     REGISTER_USER_FAIL,
+    LOAD_USER_REQUEST,
+    LOAD_USER_SUCCESS,
+    LOAD_USER_FAIL,
+    LOGOUT_SUCCESS,
+    LOGOUT_FAIL,
+    CLEAR_ERRORS,
 } from "../constants/userConstants";
 import axios from "axios";
 
-// Login
+// Entrar
 export const login = (email, password) => async (dispatch) => {
     try {
         dispatch({ type: LOGIN_REQUEST });
@@ -25,7 +30,7 @@ export const login = (email, password) => async (dispatch) => {
     }
 };
 
-// register
+// Registrar
 export const register = (userData) => async (dispatch) => {
     try {
         dispatch({ type: REGISTER_USER_REQUEST });
@@ -37,6 +42,27 @@ export const register = (userData) => async (dispatch) => {
             type: REGISTER_USER_FAIL,
             payload: error.response.data.message,
         });
+    }
+};
+
+// Carrega usuário
+export const loadUser = () => async (dispatch) => {
+    try {
+        dispatch({ type: LOAD_USER_REQUEST });
+        const { data } = await axios.get(`/api/v1/me`);
+        dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
+    } catch (error) {
+        dispatch({ type: LOAD_USER_FAIL, payload: error.response.data.message });
+    }
+};
+
+// Desconecta usuário
+export const logout = () => async (dispatch) => {
+    try {
+        await axios.get(`/api/v1/logout`);
+        dispatch({ type: LOGOUT_SUCCESS });
+    } catch (error) {
+        dispatch({ type: LOGOUT_FAIL, payload: error.response.data.message })
     }
 };
 
--- ecommerce-mern/front/src/reducers/userReducer.js
+++ ecommerce-mern/front/src/reducers/userReducer.js
@@ -2,27 +2,40 @@ import {
     LOGIN_SUCCESS,
     LOGIN_REQUEST,
     LOGIN_FAIL,
-    CLEAR_ERRORS,
     REGISTER_USER_REQUEST,
     REGISTER_USER_SUCCESS,
     REGISTER_USER_FAIL,
+    LOAD_USER_REQUEST,
+    LOAD_USER_SUCCESS,
+    LOAD_USER_FAIL,
+    LOGOUT_SUCCESS,
+    LOGOUT_FAIL,
+    CLEAR_ERRORS,
 } from "../constants/userConstants";
 
 export const userReducer = (state = { user: {} }, action) => {
     switch (action.type) {
         case LOGIN_REQUEST:
         case REGISTER_USER_REQUEST:
+        case LOAD_USER_REQUEST:
             return {
                 loading: true,
                 isAuthenticated: false,
             };
         case LOGIN_SUCCESS:
         case REGISTER_USER_SUCCESS:
+        case LOAD_USER_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 isAuthenticated: true,
                 user: action.payload,
+            };
+        case LOGOUT_SUCCESS:
+            return {
+                loading: false,
+                user: null,
+                isAuthenticated: false,
             };
         case LOGIN_FAIL:
         case REGISTER_USER_FAIL:
@@ -31,6 +44,19 @@ export const userReducer = (state = { user: {} }, action) => {
                 loading: false,
                 isAuthenticated: false,
                 user: null,
+                error: action.payload,
+            };
+        case LOAD_USER_FAIL:
+            return {
+                loading: false,
+                isAuthenticated: false,
+                user: null,
+                error: action.payload,
+            };
+        case LOGOUT_FAIL:
+            return {
+                ...state,
+                loading: false,
                 error: action.payload,
             };
         case CLEAR_ERRORS:

5e3cbe1da4a4e5d4d4051cc6e82e41cff9694f32
Diego Serafim <diegoserafim1@gmail.com>
2022-06-01 16:23:29 -0300

    created page update perfil user completed

--- ecommerce-mern/front/src/actions/userAction.js
+++ ecommerce-mern/front/src/actions/userAction.js
@@ -10,6 +10,9 @@ import {
     LOAD_USER_FAIL,
     LOGOUT_SUCCESS,
     LOGOUT_FAIL,
+    UPDATE_PROFILE_FAIL,
+    UPDATE_PROFILE_REQUEST,
+    UPDATE_PROFILE_SUCCESS,
     CLEAR_ERRORS,
 } from "../constants/userConstants";
 import axios from "axios";
@@ -62,7 +65,22 @@ export const logout = () => async (dispatch) => {
         await axios.get(`/api/v1/logout`);
         dispatch({ type: LOGOUT_SUCCESS });
     } catch (error) {
-        dispatch({ type: LOGOUT_FAIL, payload: error.response.data.message })
+        dispatch({ type: LOGOUT_FAIL, payload: error.response.data.message });
+    }
+};
+
+// Atualiza Perfil do usuário
+export const updateProfile = (userData) => async (dispatch) => {
+    try {
+        dispatch({ type: UPDATE_PROFILE_REQUEST });
+        const config = { headers: { "Content-Type": "multipart/form-data" } };
+        const { data } = await axios.put(`/api/v1/me/update`, userData, config);
+        dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.success });
+    } catch (error) {
+        dispatch({
+            type: UPDATE_PROFILE_FAIL,
+            payload: error.response.data.message,
+        });
     }
 };
 
